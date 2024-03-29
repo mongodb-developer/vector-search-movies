7af24cbc4f7a24ff201683dbe324433fefd66967
khuaulme <karen.huaulme@gmail.com>
2024-03-29 15:57:20 +0100

    background Home Page - updates Routes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className="min-h-screen bg-black"><h2 className="text-center text-4xl text-white pt-12">
+    Atlas Vector Search Movies
+  </h2>
+  </div>
   )
 }
 
